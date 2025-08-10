1156e30c24e940ce1051e3eba174dcd1a4a50ad6
Mahdi <m.jorati1384@gmail.com>
2025-08-10 21:34:30 +0330

    Initial commit from top-level folder

---
+++ {40319403}-{40323103}-{2}/Main.py
@@ -0,0 +1,11 @@
+"""
+RoadMap:
+1.Generating two large prime numbers and applying "Miller-Rabin" test.
+2.Calculating Euler functions.
+3.Creating public key.
+4.Creating privet key (using Euclidean Algorithm).
+5.Encryption & decryption.
+6.Applying primary tests.
+7.Carrying possible attacks & security checks.
+"""
+
